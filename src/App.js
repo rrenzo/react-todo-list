82170e957c1c322749efb86012be367cd65afb50
rrenzo <145371176+rrenzo@users.noreply.github.com>
2023-11-05 14:45:55 -0500

    commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {useState} from "react";
+import './App.css';
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
+function App() {
+  
+  const[todos, setTodos] = useState([])
+
+  function addTodo(todo) {
+    setTodos([todo, ...todos]);
+  }
+  
+  function toggleComplete(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    const newTodos = todos.filter((item) => item.id !== id)
+         setTodos(newTodos)
+      }    
+
+  return (
+    <header className = "App-header">
+    <div className="App">
+      <p>React Todo</p>
+      <TodoForm addTodo={addTodo}/>
+      <TodoList todos={todos} 
+      removeTodo={removeTodo}
+      toggleComplete={toggleComplete}/>
+    </div>
+    </header>
+  );
+}
+
+export default App;
